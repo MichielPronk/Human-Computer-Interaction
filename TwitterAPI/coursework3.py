31bb3df57ca01c1a220b76d3c132651aa558dc64
jlogtenberg <logtenbergjesmer@gmail.com>
2021-03-25 17:12:26 +0100

    Made the control panel

--- TwitterAPI/coursework3.py
+++ TwitterAPI/coursework3.py
@@ -1,7 +1,10 @@
 import tweepy
 import tkinter as tk
+from tkinter import messagebox
 import tkinter.ttk as ttk
 import threading
+import queue
+from geopy.geocoders import Nominatim
 
 
 class MainProgram(tk.Frame):
@@ -63,32 +66,92 @@ class TweetExtractor(tk.Frame):
     def __init__(self, parent):
         tk.Frame.__init__(self, parent)
 
+        # Initial state of the stream
         self.is_paused = True
 
+        # Get the credentials
         credentials = self.getCredentials()
+
+        # Login to twitter
         auth = tweepy.OAuthHandler(credentials[0], credentials[1])
         auth.set_access_token(credentials[2], credentials[3])
-
         self.api = tweepy.API(auth)
 
+        # Create stream frame
         self.conversation_tree = ttk.Treeview(self)
+
+        # Create control panel frame
         self.control_panel = tk.Frame(self)
 
-        self.stream_button = tk.Button(self.control_panel, text='Start/Stop', command=self.start_stop)
-        self.filter_bar = tk.Entry(self.control_panel)
-        self.filter_bar.config(width=30)
+        # Create stop and start button
+        self.stream_button_text = tk.StringVar()
+        self.stream_button_text.set("Start stream")
+        self.stream_button = tk.Button(self.control_panel, textvariable=self.stream_button_text,
+                                       command=self.start_stop)
+        self.stream_button.config(font='Helvetica 20')
+
+        # Create drop down menu
         self.option_list = ['English', 'Spanish', 'Italian', 'French', 'Japanese', 'Russian', 'Tamil']
-        options = tk.StringVar(self)
-        options.set(self.option_list[0])
-        self.language_menu = tk.OptionMenu(self.control_panel, options, *self.option_list)
-        self.language_menu.config(width=8)
-        self.lang_label = tk.Label(self.control_panel, text='Choose language')
+        self.option = tk.StringVar(self)
+        self.option.set(self.option_list[0])
+        self.language_menu = tk.OptionMenu(self.control_panel, self.option, *self.option_list)
+        self.language_menu.config(width=8, font="Helvetica 20")
+
+        # Create label for drop down menu
+        self.lang_label = tk.Label(self.control_panel, text='Choose language', font="Helvetica 20")
+
+        # Create filter list
+        self.filter_list_internal = []
         self.filter_list = ttk.Treeview(self.control_panel, columns=1)
         self.filter_list.heading("1", text="Filters")
-        self.filter_list.column("1")
+        self.filter_list.column("1", anchor=tk.CENTER)
         self.filter_list['show'] = 'headings'
 
-        # Initialize the grid
+        # Create entry field for filter list
+        self.filter_bar = tk.Entry(self.control_panel, font="Helvetica 20", justify="center")
+        self.filter_bar.bind('<Return>', self.check_filter_input)
+
+        # Create label for entry field for filter list
+        self.filter_label = tk.Label(self.control_panel, text="Enter filter terms", font="Helvetica 20")
+
+        # Create entry field for location
+        self.location_input = tk.Entry(self.control_panel, font="Helvetica 20", justify="center")
+        self.location_input.bind('<Return>', self.check_location_input)
+
+        # Create label for location
+        self.loc_label = tk.Label(self.control_panel, text="Specify location", font="Helvetica 20")
+
+        # Create label for current location
+        self.location = tk.StringVar()
+        self.location.set("Enter a location")
+        self.loc_cur_label = tk.Label(self.control_panel, textvariable=self.location, font="Helvetica 10")
+        self.loc_cur_label.config(width=60, fg="black")
+
+        # Create scale for location radius
+        self.radius = tk.IntVar()
+        self.radius.set(int(50))
+        self.radius_bar = tk.Scale(self.control_panel, from_=1, to=100, resolution=1, orient=tk.HORIZONTAL,
+                                   variable=self.radius)
+        self.radius_bar.config(width=30, length=200)
+
+        # Create label for location radius bar
+        self.radius_label = tk.Label(self.control_panel, text="Specify radius (km)", font="Helvetica 20")
+
+        # Create location queue
+        self.location_queue = queue.Queue()
+
+        # Create longitude and latitude variables
+        self.longitude = float()
+        self.latitude = float()
+
+        # Create delete button
+        self.delete_button = tk.Button(self.control_panel, text="Delete", font="Helvetica 20", command=self.delete)
+
+        # Create informative label for stream button
+        self.warning = tk.Label(self.control_panel, text="You cannot change the settings once the stream is running.",
+                                font="Helvetica 16")
+
+        # Initialize weights for the main frame
         self.grid(column=0, row=0, sticky='NESW')
         tk.Grid.rowconfigure(parent, 0, weight=1)
         tk.Grid.columnconfigure(parent, 0, weight=1)
@@ -97,24 +160,38 @@ class TweetExtractor(tk.Frame):
         for columns in range(5):
             tk.Grid.columnconfigure(self, columns, weight=1)
 
+        # Insert stream frame into main frame
         self.conversation_tree.grid(column=0, row=0, columnspan=4, rowspan=1, sticky='NESW')
+
+        # Insert control panel into main frame
         self.control_panel.grid(column=4, row=0, columnspan=1, rowspan=1, sticky='NESW')
 
+        # Initialize weights for control panel frame
         tk.Grid.rowconfigure(self.control_panel, 0, weight=1)
         tk.Grid.columnconfigure(self.control_panel, 0, weight=1)
-        for columns in range(30):
-            tk.Grid.columnconfigure(self.control_panel, columns, weight=2)
-        for rows in range(30):
-            tk.Grid.rowconfigure(self.control_panel, rows, weight=2)
-        self.filter_list.grid(column=1, row=0, columnspan=28, rowspan=6, sticky='NSEW')
-        self.filter_bar.grid(column=2, row=6, columnspan=10, sticky='EW')
-        self.language_menu.grid(column=1, row=9, sticky='EW')
-        self.stream_button.grid(column=1, row=18, sticky='EW')
-        self.lang_label.grid(column=1, row=8)
+        for columns in range(7):
+            tk.Grid.columnconfigure(self.control_panel, columns, weight=1)
+        for rows in range(50):
+            tk.Grid.rowconfigure(self.control_panel, rows, weight=1)
+
+        # Insert control panel options
+        self.filter_list.grid(column=1, row=0, columnspan=5, rowspan=26, sticky='NSEW')
+        self.delete_button.grid(column=3, row=27)
+        self.filter_label.grid(column=3, row=28)
+        self.filter_bar.grid(column=3, row=29)
+        self.loc_label.grid(column=3, row=33)
+        self.location_input.grid(column=3, row=34)
+        self.loc_cur_label.grid(column=3, row=35)
+        self.radius_label.grid(column=3, row=36)
+        self.radius_bar.grid(column=3, row=37)
+        self.lang_label.grid(column=3, row=38)
+        self.language_menu.grid(column=3, row=39)
+        self.stream_button.grid(column=3, row=41, sticky='EW')
+        self.warning.grid(column=3, row=43)
 
     def getCredentials(self):
         credentials = []
-        with open("credentials", "r") as infile:
+        with open("credentials2", "r") as infile:
             lines = infile.readlines()
         credentials.append(lines[0].strip().split(":")[1])
         credentials.append(lines[1].strip().split(":")[1])
@@ -132,14 +209,110 @@ class TweetExtractor(tk.Frame):
             if myStream.running and self.is_paused:
                 myStream.disconnect()
             elif not myStream.running and not self.is_paused:
-                print(self.filter_bar.get())
-                myStream.filter(is_async=True, track=['python'])
+                language = self.getLanguage()
+                location = self.getLocation()
+                if location is None:
+                    myStream.filter(is_async=True, track=self.filter_list_internal, languages=[language])
+                else:
+                    myStream.filter(is_async=True, track=self.filter_list_internal, languages=[language], locations=location)
 
     def start_stop(self):
         if self.is_paused:
-            self.is_paused = False
+            if not self.filter_list_internal:
+                tk.messagebox.showerror("Error", "Please specify at least one filter in the filter list")
+            else:
+                self.is_paused = False
+                self.delete_button.config(state=tk.DISABLED)
+                self.filter_bar.config(state=tk.DISABLED)
+                self.location_input.config(state=tk.DISABLED)
+                self.radius_bar.config(state=tk.DISABLED)
+                self.language_menu.config(state=tk.DISABLED)
+                self.stream_button_text.set("Stop stream")
         else:
             self.is_paused = True
+            self.delete_button.config(state=tk.NORMAL)
+            self.filter_bar.config(state=tk.NORMAL)
+            self.filter_bar.delete(0, tk.END)
+            self.location_input.config(state=tk.NORMAL)
+            self.location_input.delete(0, tk.END)
+            self.radius_bar.config(state=tk.NORMAL)
+            self.language_menu.config(state=tk.NORMAL)
+
+            self.filter_list_internal = []
+            self.filter_list.delete(*self.filter_list.get_children())
+
+            self.longitude = float()
+            self.latitude = float()
+
+            self.location.set("Enter a location")
+            self.loc_cur_label.config(fg="black")
+
+            self.stream_button_text.set("Start stream")
+
+    def check_filter_input(self, event):
+        input = self.filter_bar.get()
+        self.filter_bar.delete(0, tk.END)
+        if input == "":
+            tk.messagebox.showerror("Error", "Filter field is empty")
+        elif input in self.filter_list_internal:
+            tk.messagebox.showerror("Error", "Filter already exists")
+        else:
+            self.filter_list_internal.append(input)
+            self.filter_list.insert("", tk.END, text=input, values=(input,))
+
+    def check_location_input(self, event):
+        input = self.location_input.get()
+        self.location_input.delete(0, tk.END)
+        if input == "":
+            tk.messagebox.showerror("Error", "Location field is empty")
+        elif input == self.location:
+            tk.messagebox.showerror("Error", "Location is the same as the set location")
+        else:
+            self.location_queue.put(input)
+
+    def startLocationChecker(self):
+        threading.Thread(target=self.location_checker).start()
+
+    def location_checker(self):
+        geolocator = Nominatim(user_agent="JeMiBot")
+        while True:
+            try:
+                item = self.location_queue.get(block=False)
+                try:
+                    location = geolocator.geocode(item)
+                    self.longitude = location.longitude
+                    self.latitude = location.latitude
+                    self.location.set(location.address)
+                    self.loc_cur_label.config(fg="green")
+                except AttributeError:
+                    self.location.set("Location was not found")
+                    self.loc_cur_label.config(fg="red")
+            except queue.Empty:
+                pass
+
+    def delete(self):
+        row_id = self.filter_list.focus()
+        node_name = self.filter_list.item(row_id)['text']
+        try:
+            self.filter_list.delete(row_id)
+            self.filter_list_internal.remove(node_name)
+        except tk.TclError:
+            pass
+
+    def getLanguage(self):
+        selected = self.option.get()
+        codes = {'English': 'en', 'Italian': 'it', 'French': 'fr', 'Spanish': 'es', 'Russian': 'ru', 'Japanese': 'jp',
+                 'Tamil': 'ta'}
+        return codes[selected]
+
+    def getLocation(self):
+        if self.longitude == 0 and self.latitude == 0:
+            return None
+        else:
+            east_west = self.radius.get() * 1000 * 0.5 / 0.999984984 / 111111
+            north_south = self.radius.get() * 1000 * 0.866025 / 111111
+            return [self.longitude - north_south - east_west, self.latitude - north_south - east_west, self.longitude + north_south + east_west, self.latitude + north_south + east_west]
+
 
 class SentimentAnalysis(tk.Frame):
     def __init__(self, parent):
@@ -165,7 +338,6 @@ class SentimentAnalysis(tk.Frame):
 
 
 class MyStreamListener(tweepy.StreamListener):
-
     def on_status(self, tweet):
         print(tweet.text)
 
@@ -175,6 +347,7 @@ def main():
     root.state('zoomed')
     frame = MainProgram(root)
     frame.tweet_extractor.startStream()
+    frame.tweet_extractor.startLocationChecker()
     root.mainloop()
 
 
